c88030611e411f489bb80a992648bcb0031a6b6f
zolboo1024 <38169804+zolboo1024@users.noreply.github.com>
2018-11-19 23:26:35 -0500

    Update Browser.java

--- app/src/main/java/edu/dickinson/newmixxer/Browser.java
+++ app/src/main/java/edu/dickinson/newmixxer/Browser.java
@@ -9,7 +9,8 @@ import android.widget.Toast;
 
 /**
  * Created by Zolboo Erdenebaatar 11/19/2018
- *
+ * This is a class that basically implements the 
+ * core part of the application: The WebView component.
  */
 public class Browser extends AppCompatActivity {
     WebView webView;
@@ -18,19 +19,27 @@ public class Browser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browser);
-        webView = (WebView) findViewById(R.id.webView);
+        // WebView is just like any other Android Components like buttons or imageview
+        webView = (WebView) findViewById(R.id.webView); 
+        // Must enable JS and Cache 
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setAppCacheEnabled(true);
         Intent intent= getIntent();
+        // Here, we get URL that is passed from the MainActivity 
+        // as an extra string from the intent.
         url= intent.getStringExtra("url");
         callUrl(url);
     }
+    // This method basically loads the URL and handles an exception. Once 
+    // an exception is thrown it automatically goes back to the main 
+    // starting point of the application
     protected void callUrl(String url){
         webView.loadUrl(url);
         webView.setWebViewClient(new WebViewClient(){
 
             public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-
+                   //Basically, when an error is thrown, displays a message and goes back to the Activity
+                    // that called this Activity.
                 Intent intent= new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
                 Toast.makeText(getApplicationContext(), "No Internet Connection. Try Again Later", Toast.LENGTH_SHORT).show();
