9ca9675bfb372983329a09f8215de2da083bdd7a
zolboo1024 <38169804+zolboo1024@users.noreply.github.com>
2018-11-19 23:28:31 -0500

    Update MainActivity.java

--- app/src/main/java/edu/dickinson/newmixxer/MainActivity.java
+++ app/src/main/java/edu/dickinson/newmixxer/MainActivity.java
@@ -7,30 +7,24 @@ import android.view.View;
 import android.widget.Button;
 /**
  * Created by Zolboo Erdenebaatar 11/19/2018
- *
+ * This Class is the entry point of the application.   
+ * Just for an example, this Activity creates a button
+ * when pressed, opens up the WebView. 
  */
 public class MainActivity extends AppCompatActivity {
     Button signUp;
-    Button logIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        signUp= (Button) findViewById(R.id.signUp);
         logIn= (Button) findViewById(R.id.logIn);
-        signUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent browserIntent = new Intent(getApplicationContext(), Browser.class);
-                browserIntent.putExtra("url", "https://mixxertestdev.dickinson.edu/user/register");
-                startActivity(browserIntent);
-            }
-        });
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
+            // This method calls the Browser class which basically contains the 
+            // the implementation of the Actual WebView.
             public void onClick(View view) {
                 Intent browserIntent = new Intent(getApplicationContext(), Browser.class);
-                browserIntent.putExtra("url", "https://mixxertestdev.dickinson.edu/user/login");
+                browserIntent.putExtra("url", "https://www.google.com");
                 startActivity(browserIntent);
             }
         });
